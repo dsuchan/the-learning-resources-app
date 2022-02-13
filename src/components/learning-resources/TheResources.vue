1fe5a2eebed5bae33081d20717341aa77951eeac
David Suchan <dav.suchan@gmail.com>
2022-02-13 12:17:21 +0100

    added addResource method

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -11,7 +11,10 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <!-- Caching the inputed data in this component so they doesn't get lost -->
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -44,7 +47,10 @@ export default {
   },
   provide() {
     return {
+      // Will get injected to the 'StoredResources.vue' component
       resources: this.storedResources,
+      // Will get injected to the 'AddResource.vue' component
+      addResource: this.addResource,
     };
   },
   computed: {
@@ -58,6 +64,19 @@ export default {
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    // Creating new resource and storing it at the top of 'storedResources' arr
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      // 'unshift' method will put the 'newResource' at the top of arr
+      this.storedResources.unshift(newResource);
+      // Then switching the selected tab to 'stored-resources'
+      this.selectedTab = 'stored-resources';
     },
   },
 };
